55c6562257ddf7a969c173abba664f11bd114117
Mukesh Behera <mukesh.contractorplus@gmail.com>
2025-07-31 14:32:42 +0530

    its react web project

---
+++ suSarjana/src/App.jsx
@@ -0,0 +1,25 @@
+import {StrictMode,useState,useEffect} from 'react'
+
+function App(){
+
+
+const userName="amit Behera";
+let x=28;
+let y=129;
+return(
+<div>  
+<input type='radio' name='gender' id='male' />male
+<input type='radio' name='gender' id='female' />female
+<h1>  web panel in the house</h1>
+<h2 style={{color:'yellow', fontSize:100}} > direct on the shelter     of     Narsimha Dev    i am prahalad       </h2>
+
+</div>
+
+
+
+)
+}
+
+
+
+export default App;
